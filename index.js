dde8c4b97e2ea3c49f489891c2f858f4374bdbdb
sanashahbaz27 <115797100+sanashahbaz27@users.noreply.github.com>
2022-11-15 19:41:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! I am changed 2\n'
+  const msg = 'Hello Node! CICD with jenkins\n'
   res.end(msg);
 });
 
